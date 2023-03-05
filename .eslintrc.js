fbea7e559d1091201bf639101f7ad98774abf438
Yosuke Iida <hitu3690@icloud.com>
2023-03-05 10:30:40 +0900

    path alias

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2019, // Node.js 12の場合は2019、他のバージョンのNode.jsを利用している場合は場合は適宜変更する
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.eslint.json"],
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/no-unsafe-assignment": 1,
+    "@typescript-eslint/no-unsafe-member-access": 1,
+    "@typescript-eslint/no-unsafe-call": 1,
+  },
+  // settings: {
+  //   "import/resolver": {
+  //     typescript: {},
+  //   },
+  // },
+};
